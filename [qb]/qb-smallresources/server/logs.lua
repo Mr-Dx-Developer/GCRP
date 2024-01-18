c9ac94ae592e5228fc3ac51ddcd3ac46b1a00abb
Tanvir Anjum Neon <taneon98@gmail.com>
2024-01-18 10:19:45 -0600

    small

--- [qb]/qb-smallresources/server/logs.lua
+++ [qb]/qb-smallresources/server/logs.lua
@@ -1,9 +1,5 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-
 local Webhooks = {
-
-    ['qbjobs'] = '',
-    ['humanelabs'] = '',
     ['default'] = '',
     ['testwebhook'] = '',
     ['playermoney'] = '',
@@ -41,21 +37,10 @@ local Webhooks = {
     ['911'] = '',
     ['palert'] = '',
     ['house'] = '',
-    ['granny'] = '',
-    ['truckrobbery'] = '',
-    ['varheist'] = '',
-    ['hunting'] = '',
-    ['gamble'] = '',
-    ['scenes'] = '',
-    ['gundrop'] = '',
-    ['gangmenu'] = '',
-    ['warehouse'] = '',
-    ['pdarmory'] = '',
-    ['evidence'] = '',
-    ['donjewellery'] = '',
+    ['qbjobs'] = '',
 }
 
-local Colors = { -- https://www.spycolor.com/
+local colors = { -- https://www.spycolor.com/
     ['default'] = 14423100,
     ['blue'] = 255,
     ['red'] = 16711680,
@@ -65,30 +50,64 @@ local Colors = { -- https://www.spycolor.com/
     ['orange'] = 16744192,
     ['yellow'] = 16776960,
     ['pink'] = 16761035,
-    ["lightgreen"] = 65309,
+    ['lightgreen'] = 65309,
 }
 
+local logQueue = {}
+
 RegisterNetEvent('qb-log:server:CreateLog', function(name, title, color, message, tagEveryone)
+    local postData = {}
     local tag = tagEveryone or false
-    local webHook = Webhooks[name] or Webhooks['default']
+    if not Webhooks[name] then print('Tried to call a log that isn\'t configured with the name of ' ..name) return end
+    local webHook = Webhooks[name] ~= '' and Webhooks[name] or Webhooks['default']
     local embedData = {
         {
             ['title'] = title,
-            ['color'] = Colors[color] or Colors['default'],
+            ['color'] = colors[color] or colors['default'],
             ['footer'] = {
                 ['text'] = os.date('%c'),
             },
             ['description'] = message,
             ['author'] = {
                 ['name'] = 'QBCore Logs',
-                ['icon_url'] = 'https://media.discordapp.net/attachments/870094209783308299/870104331142189126/Logo_-_Display_Picture_-_Stylized_-_Red.png?width=670&height=670',
+                ['icon_url'] = 'https://raw.githubusercontent.com/GhzGarage/qb-media-kit/main/Display%20Pictures/Logo%20-%20Display%20Picture%20-%20Stylized%20-%20Red.png',
             },
         }
     }
-    PerformHttpRequest(webHook, function() end, 'POST', json.encode({ username = 'QB Logs', embeds = embedData}), { ['Content-Type'] = 'application/json' })
-    Citizen.Wait(100)
-    if tag then
-        PerformHttpRequest(webHook, function() end, 'POST', json.encode({ username = 'QB Logs', content = '@everyone'}), { ['Content-Type'] = 'application/json' })
+
+    if not logQueue[name] then logQueue[name] = {} end
+    logQueue[name][#logQueue[name] + 1] = {webhook = webHook, data = embedData}
+
+    if #logQueue[name] >= 10 then
+        if tag then
+            postData = {username = 'QB Logs', content = '@everyone', embeds = {}}
+        else
+            postData = {username = 'QB Logs', embeds = {}}
+        end
+        for i = 1, #logQueue[name] do postData.embeds[#postData.embeds + 1] = logQueue[name][i].data[1] end
+        PerformHttpRequest(logQueue[name][1].webhook, function() end, 'POST', json.encode(postData), { ['Content-Type'] = 'application/json' })
+        logQueue[name] = {}
+    end
+end)
+
+Citizen.CreateThread(function()
+    local timer = 0
+    while true do
+        Wait(1000)
+        timer = timer + 1
+        if timer >= 60 then -- If 60 seconds have passed, post the logs
+            timer = 0
+            for name, queue in pairs(logQueue) do
+                if #queue > 0 then
+                    local postData = {username = 'QB Logs', embeds = {}}
+                    for i = 1, #queue do
+                        postData.embeds[#postData.embeds + 1] = queue[i].data[1]
+                    end
+                    PerformHttpRequest(queue[1].webhook, function() end, 'POST', json.encode(postData), {['Content-Type'] = 'application/json'})
+                    logQueue[name] = {}
+                end
+            end
+        end
     end
 end)
 
