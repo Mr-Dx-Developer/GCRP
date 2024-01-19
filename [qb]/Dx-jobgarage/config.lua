918274d26406cb407c14dd71b492fa2ce1f95383
Tanvir Anjum Neon <taneon98@gmail.com>
2024-01-19 12:33:58 +0600

    job garage

---
+++ [qb]/Dx-jobgarage/config.lua
@@ -0,0 +1,490 @@
+print("^2Jim^7-^2JobGarage ^7v^41^7.^44^7.^43 ^7- ^2Job Garage Script by ^1Jimathy^7")
+
+Loc = {}
+
+--[[	LIST OF POSSIBLE VEHICLE MODIFIERS   ]]--
+-- Using these will change how each vehicle spawns
+-- This can be used for making sure the vehicles comes out exactly how you want it to
+
+-- CustomName = "Police Car", this will show a custom override name for your vehicles so you don't need to add them to your vehicles.lua
+-- rank = { 2, 4 }, -- This specifes which grades can see it, and only these grades
+-- grade = 4, -- This specifies the lowest grade and above that can see the vehicle
+-- colors = { 136, 137 }, -- This is the colour index id of the vehicle, Primary and Secondary in that order
+-- bulletproof = true, -- This determines if the tyres are bullet proof (don't ask me why, I was asked to add this)
+-- livery = 1, -- This sets the livery id of the vehicle, (most mod menus would number them or have them in number order) 0 = stock
+-- extras = { 1, 5 }, -- This enables the selected extras on the vehicle
+-- performance = "max", this sets the stats to max if available
+-- performance = { 2, 3, 3, 2, 4, true }, -- This allows more specific settings for each upgrade level, in order: engine, brakes, suspension, transmission, armour, turbo
+-- trunkItems = { }, -- Use this to add items to the trunk of the vehicle when it is spawned
+
+-- ANY VEHICLE, BOATS, POLICE CARS, EMS VEHICLES OR EVEN PLANES CAN BE ADDED.
+
+Config = {
+	Debug = false,  -- Enable to use debug features
+	Lan = "en",
+
+	Core = "qb-core",
+	Menu = "qb",
+	Notify = "qb",
+
+	Fuel = "LegacyFuel", -- Set this to your fuel script folder
+
+	CarDespawn = false, -- Sends the vehicle to hell (removal animation)
+
+	DistCheck = false, -- Require the vehicle to be near by to remove it
+
+	Locations = {
+		{    zoneEnable = true, -- disable if you want to hide this temporarily
+			job = "mechanic", -- set this to required job grade
+			garage = {
+				spawn = vec4(-362.97, -122.45, 38.7, 153.84),  -- Where the vehicle will spawn
+				out = vec4(-353.55, -125.96, 39.43, 67.7),  -- Where you select the vehicles from
+				list = {
+					["flatbed"] = {
+						CustomName = "Flatbed",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+							
+							
+						},
+					}, 
+					
+				},
+			},
+		},
+		--[[ { 	zoneEnable = true,
+			job = "mechanic",
+			garage = {
+				spawn = vec4(-179.34, -1285.27, 30.83, 89.24),  -- Where the car will spawn
+				out = vec4(-177.1, -1282.25, 31.3, 179.01),	-- Where the parking stand is
+				list = {
+					["cheburek"] = {
+						colors = { 136, 137 },
+						grade = 4,
+						livery = 5,
+						bulletproof = true,
+						extras = { 1, 4 },
+					},
+					["burrito3"] = { },
+				},
+			},
+		}, ]]
+		{ 	zoneEnable = true,
+			job = "police",
+			garage = {
+				spawn = vec4(431.16, -981.02, 25.14, 182.32),
+				out = vec4(427.78, -988.33, 25.14, 269.93),
+				list = {
+					["polmustang"] = {
+						CustomName = "polmustang",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["rmodgt63police"] = {
+						CustomName = "rmodgt63police",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["sw_subrb"] = {
+						CustomName = "sw_subrb",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["unmarkedjl"] = {
+						CustomName = "unmarkedjl",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["zm_s500"] = {
+						CustomName = "zm_s500",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["zm_rocket900"] = {
+						CustomName = "zm_rocket900",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["trhawk"] = {
+						CustomName = "trhawk",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["sw_bearcat"] = {
+						CustomName = "sw_bearcat",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["polkawasaki"] = {
+						CustomName = "polkawasaki",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["poldurango"] = {
+						CustomName = "poldurango",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["polcross"] = {
+						CustomName = "polcross",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["polboat2"] = {
+						CustomName = "polboat2",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["polcharger18"] = {
+						CustomName = "polcharger18",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["polboat1"] = {
+						CustomName = "polboat1",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["polbmwm7"] = {
+						CustomName = "polbmwm7",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["pd_escalader"] = {
+						CustomName = "pd_escalader",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["nm_z71"] = {
+						CustomName = "nm_z71",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["nm_ctsv"] = {
+						CustomName = "nm_ctsv",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["DL_rs6"] = {
+						CustomName = "DL_rs6",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["DL_a45"] = {
+						CustomName = "DL_a45",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["ACTRPavantum"] = {
+						CustomName = "ACTRPavantum",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["ACTRPavant"] = {
+						CustomName = "ACTRPavant",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["22m5sal"] = {
+						CustomName = "22m5sal",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					
+					--[[ ["fbi"] = {
+						rank = { 4 },
+						performance = { 2, 3, 3, 2, 4, true },
+					}, ]]
+				},
+			},
+		},
+		{    zoneEnable = true, -- disable if you want to hide this temporarily
+			job = "police", -- set this to required job grade
+			garage = {
+				spawn = vec4(447.65, -981.21, 43.69, 93.61),  -- Where the vehicle will spawn
+				out = vec4(463.93, -980.53, 43.69, 97.02),  -- Where you select the vehicles from
+				list = {  -- The list of cars that will spawn
+					["eheli"] = {
+						CustomName = "eheli",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+					},
+					["fibheli"] = {
+						CustomName = "fibheli",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+					},
+					["polheli"] = {
+						CustomName = "polheli",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+					}
+				},
+			}, 
+		},	
+		--[[ { 	zoneEnable = true,
+			job = "ambulance",
+			garage = {
+				spawn = vec4(294.95, -606.92, 43.24, 64.84),
+				out = vec4(294.54, -599.52, 43.28, 159.68),
+				list = {
+					["ambulance"] = {
+						CustomName = "Ambulance"
+					},
+				},
+			},
+		}, ]]
+		{ 	zoneEnable = true,
+			job = "ambulance",
+			garage = {
+				spawn = vec4(-489.14, -331.79, 34.36, 264.08),
+				out = vec4(-486.13, -325.69, 34.5, 170.12),
+				list = {
+					
+					["emsbmwm7"] = {
+						CustomName = "emsbmwm7",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["Ambulance_Dodge_RAM3"] = {
+						CustomName = "Ambulance_Dodge_RAM3",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["RoyalCustom_RSQ8_EMS"] = {
+						CustomName = "RoyalCustom_RSQ8_EMS",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["RoyalG30Medic"] = {
+						CustomName = "RoyalG30Medic",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["DC_RoyalCustom___RS7_EMS"] = {
+						CustomName = "DC_RoyalCustom___RS7_EMS",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["BMW-x5_medic"] = {
+						CustomName = "BMW-x5_medic",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					["aw139"] = {
+						CustomName = "aw139",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+						trunkItems = {
+						--	{ name = "heavyarmor", amount = 2, info = {}, type = "item", slot = 1, },
+							{ name = "empty_evidence_bag", amount = 10, info = {}, type = "item", slot = 2, },
+							{ name = "police_stormram", amount = 1, info = {}, type = "item", slot = 3, },
+						},
+					},
+					
+					
+					--[[ ["fbi"] = {
+						rank = { 4 },
+						performance = { 2, 3, 3, 2, 4, true },
+					}, ]]
+				},
+			},
+		},
+		{    zoneEnable = true, -- disable if you want to hide this temporarily
+			job = "ambulance", -- set this to required job grade
+			garage = {
+				spawn = vec4(-451.11, -307.06, 78.17, 25.51),  -- Where the vehicle will spawn
+				out = vec4(-441.74, -309.38, 78.17, 109.59),  -- Where you select the vehicles from
+				list = {  -- The list of cars that will spawn
+					["aw139"] = {
+						CustomName = "aw139",
+						livery = 0,
+						extras = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+						performance = "max",
+					}
+					
+				},
+			}, 
+		},
+	},
+}
