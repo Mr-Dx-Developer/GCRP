686e746408cf41b00f92e08e27737d37c91f2a44
Tanvir Anjum Neon <taneon98@gmail.com>
2024-01-24 07:04:16 -0600

    carExp

---
+++ [GCRP]/[Fun]/wais-carExplosion/config.lua
@@ -0,0 +1,2 @@
+Config = {}
+Config.ExpHeight = 10 -- Minimum height at which the vehicle will explode after jumping
---
+++ [GCRP]/[Fun]/wais-carExplosion/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+game 'gta5'
+Author 'Ayazwai#3900'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+lua54 'yes'
