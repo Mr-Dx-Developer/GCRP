0747e91e8ccf95ad603e7f3f828c68ba50b8f39b
Tanvir Anjum Neon <taneon98@gmail.com>
2024-01-25 20:58:56 -0600

    remove nass server

--- [GCRP]/[Basic]/nass_serverstore/inventoryitems/ESX/limit.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `items` (`name`, `label`, `limit`) VALUES
-	('platechanger', 'Plate Changer', 1),
-	('namechanger', 'Name Changer', 1)
-;
--- [GCRP]/[Basic]/nass_serverstore/inventoryitems/ESX/weight.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `items` (`name`, `label`, `weight`) VALUES
-	('platechanger', 'Plate Changer', 1),
-	('namechanger', 'Name Changer', 1)
-;
